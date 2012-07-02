a9ca864870b6c0db25941ac4967009e5cb992fd9
OGURA Daiki <ogura@aimu.co.jp>
2012-07-02 17:21:58 +0900

    UI tests

---
+++ HachiValidater/CheckConfigs/Omni.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace HachiValidater.CheckConfigs
+{
+	public class Omni:CheckConfigs.IChecker<System.Windows.Forms.TextBox>
+	{
+		private Regex reg;
+
+		public Omni()
+		{
+		}
+
+		public Omni(string re)
+		{
+			reg = new Regex(re);
+		}
+
+		public bool isConfirmed(System.Windows.Forms.TextBox targ)
+		{
+			if (targ.Text == "") { return true; } //donot requier check
+			return reg.IsMatch(targ.Text);
+		}
+	}
+}
--- testUI/Form1.cs
+++ testUI/Form1.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
+using HachiValidater.CheckConfigs;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -11,6 +11,11 @@ namespace testUI
 {
 	public partial class Form1 : Form
 	{
+
+		private HachiValidater.CheckConfigs.ValidateContainer container1;
+		private HachiValidater.CheckConfigs.ValidateContainer container2;
+
+
 		public Form1()
 		{
 			InitializeComponent();
@@ -19,6 +24,19 @@ namespace testUI
 		private void Form1_Load(object sender, EventArgs e)
 		{
 			textBox1.isCanceled = true;
+			container1 = 
+				new HachiValidater.CheckConfigs.ValidateContainer(new Omni(@"^\d+$"),"数字オンリー");
+			container2 = 
+				new HachiValidater.CheckConfigs.ValidateContainer(new Omni(@"^[a-z]+$"),"a～zだけ");
+			container1.controls = new List<Control> { textBox2, textBox3 };
+			container2.controls = new List<Control> { textBox4 };
+			button1.containeres = new List<ValidateContainer> { container1, container2 };
+			button1.errorProvider = errorProvider1;
+		}
+
+		private void button1_afterComfirmed(object sender, EventArgs e)
+		{
+			MessageBox.Show("せい！");
 		}
 	}
 }
